9de09ce25aa10871339c1a539375c217b9300500
Houssem <houssem@fffff.speedport.ip>
2018-04-07 16:46:58 +0200

    ignore

--- EVLIN_EDBT/src/main/java/henry/QuerySimilarity/ISimilarity.java
+++
@@ -1,5 +0,0 @@
-package henry.QuerySimilarity;
-
-public interface ISimilarity {
-	public double calculateSimilarity(QueryFromDB q1 , QueryFromDB q2);
-}
--- EVLIN_EDBT/src/main/java/henry/database/connection/package-info.java
+++
@@ -1,7 +0,0 @@
-
-
-@org.hibernate.annotations.TypeDef(name = "MyDisplayFormType", typeClass = MyDisplayFormType.class)
-
-package henry.database.connection;
-
-import prov.mybean.MyDisplayFormType;
